971a7fa6763441fe2f91bc4b680dd64deeb8c4fc
hamdyWahab <hamdywahab16@gmail.com>
2025-04-29 01:02:26 -0700

    Add files via upload

---
+++ Day1exe.js
@@ -0,0 +1,29 @@
+let price = 100;
+const taxRate =0.1
+let quantity =3; 
+totalcost =(price*quantity*(1+taxRate))
+quantity =5;
+totalcost1 =(price*quantity*(1+taxRate))
+console.log(totalcost);
+console.log(totalcost1)
+
+let score =95;
+let message ="Freat job!"
+let isPassing =true;
+let grades =[90,85,88];
+let student={name:"Emma",age:20}
+console.log(typeof(score));
+console.log(typeof(message));
+console.log(typeof(isPassing));
+console.log(typeof(grades));
+console.log(typeof(student));
+console.log(grades[1]);
+let a =10,b=3;
+let addition =a+b;
+console.log(addition);
+let modulo = a%b
+console.log(modulo)
+let raiseToPower=a**2;
+console.log(raiseToPower);
+console.log((a>5)&&(b<5))
+a+=5
